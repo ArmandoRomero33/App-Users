af1a429f95b5a83b710e4c7e336844fd03603234
Jose Armando Romero Orbe <armamdoromeroorbe33@gmail.com>
2021-11-23 17:25:42 -0600

    vista alternativa

--- app/src/main/java/com/app/userssp/MainActivity.kt
+++ app/src/main/java/com/app/userssp/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.app.userssp.databinding.ActivityMainBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity(), OnClickListener {
 
@@ -29,13 +30,27 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         Log.i("SP", "${getString(R.string.sp_first_time)} = $isFirstTime")
 
         if(isFirstTime) {
+            val dialogView = layoutInflater.inflate(R.layout.dialog_register, null)
+
             MaterialAlertDialogBuilder(this)
                 .setTitle(R.string.dialog_title)
+                .setView(dialogView)
+                .setCancelable(false)
                 .setPositiveButton(R.string.dialog_confirm, { dialogInterface, i ->
-                    preferences.edit().putBoolean(getString(R.string.sp_first_time), false).commit()
+                    val username = dialogView.findViewById<TextInputEditText>(R.id.etUsername)
+                        .text.toString()
+                    with(preferences.edit()){
+                        putBoolean(getString(R.string.sp_first_time), false)
+                        putString(getString(R.string.sp_username), username)
+                            .apply()
+                    }
+                    Toast.makeText(this, R.string.register_succes, Toast.LENGTH_SHORT)
+                        .show()
                 })
-                .setNegativeButton("Canelar", null)
                 .show()
+        } else{
+            val username = preferences.getString(getString(R.string.sp_username), getString(R.string.helper_username))
+            Toast.makeText(this, "Bienvenido $username", Toast.LENGTH_SHORT).show()
         }
 
         userAdapter = UserAdapter(getUsers(), this )
